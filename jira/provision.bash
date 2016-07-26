6232d2bb64ea1e014bee8d96370ef2dea0bde6a6
root <root@localhost.localdomain>
2016-07-26 19:44:11 +0100

    update download url

--- jira/provision.bash
+++ jira/provision.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ex
 
-curl -Lks https://downloads.atlassian.com/software/jira/downloads/atlassian-jira-software-${JIRA_VERSION}-jira-${JIRA_VERSION}.tar.gz -o /root/jira.tar.gz
+curl -Lks https://downloads.atlassian.com/software/jira/downloads/atlassian-jira-software-jira-${JIRA_VERSION}.tar.gz -o /root/jira.tar.gz
 /usr/sbin/useradd --create-home --home-dir /opt/jira --groups atlassian --shell /bin/bash jira
+
+
 tar zxf /root/jira.tar.gz --strip=1 -C /opt/jira
 rm /root/jira.tar.gz
 chown -R jira:jira /opt/atlassian-home
